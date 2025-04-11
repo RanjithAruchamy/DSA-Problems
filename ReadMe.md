a4122ed41bb7ea0206e029816e42132985fad64d
RanjithAruchamy <ranjith191098@gmail.com>
2025-04-11 19:34:25 +0530

    Udemy - Reverse a String [ReverseString.java]

--- ReadMe.md
+++ ReadMe.md
@@ -7,6 +7,8 @@ This repository holds the solutions for the DSA problems that I solved.
 
 ## LeetCode
 - [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/) - [RomanToInt](src/leetcode/easy/RomanToInt.java)
+- [14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/) - [LongestCommonPrefix](src/leetcode/easy/LongestCommonPrefix.java) 
 
 ## Data Structures
 - [MyArray](src/Arrays/MyArray.java)
+- [ReverseString](src/Arrays/ReverseString.java)
---
+++ src/Arrays/ReverseString.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    
+    public static String reverseString(String inputString) {
+        String[] splittedString = inputString.split("");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = splittedString.length - 1; i >= 0; i--) {
+            stringBuilder.append(splittedString[i]);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String reverseString1(String inputString) {
+        return new StringBuilder(inputString).reverse().toString();
+    }
+
+    public static String recursiveReversal (String str) {
+        if (str.isEmpty())  //breakpoint for recursion
+            return "";
+        return recursiveReversal(str.substring(1)) + str.charAt(0);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(recursiveReversal(recursiveReversal("Hi I'm Ranjith!")));
+    }
+}
